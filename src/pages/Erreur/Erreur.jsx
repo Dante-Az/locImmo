6275e1d859068499773b27d60c6adb2f1dcac19e
Jeremy Carriere <jeremyo.carriere@gmail.com>
2024-06-03 18:15:58 +0200

    test ajout page erreur + css

--- src/pages/Erreur/Erreur.jsx
+++ src/pages/Erreur/Erreur.jsx
@@ -1,8 +1,17 @@
+import {Link} from "react-router-dom"
+import styles from './Erreur.module.css'
+
 export default function Erreur() {
-    return (
-        <div>
-            <h1>Erreur</h1>
-            <p>Cette page n&apos;existe pas.</p>
+    return(
+        <div className={styles.errorBlock}>
+            <h1 className={styles.errorNumber}>404</h1>
+            <div className={styles.errorTextBlock}>
+                <h2 className={styles.errorText}>Oups! La page que&nbsp;</h2>
+                <span className={styles.errorText}>vous demandez n&apos;existe pas.</span>
+            </div>
+            <p className={styles.errorLinks}>
+                <Link to="/" className={styles.errorLink}>Retourner sur la page d’accueil</Link>
+            </p>
         </div>
-    );
+    )
 }
