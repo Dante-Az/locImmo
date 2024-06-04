1e0b7be96c422b56f3a710118796b2a5fbce7873
Dante-Az <jeremyo.carriere@gmail.com>
2024-06-04 14:39:17 +0200

    test symfony

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import image from '../../assets/IMG.jpg'
+import image from '../../assets/IMG2.jpg'
 import Banner from '../../components/Banner/Banner'
 import Cards from '../../components/Cards/Cards'
 import styles from './Home.module.css'
